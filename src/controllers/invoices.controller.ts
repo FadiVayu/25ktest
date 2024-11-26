162dc7812a1d5ada056c3c2b523f136bbed793f7
Fadi Atamny <fadi@withvayu.com>
2024-11-26 18:04:14 +0200

    added nodemon

--- src/controllers/invoices.controller.ts
+++ src/controllers/invoices.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Route, Get, Path } from 'tsoa'
+import { Controller, Route, Get, Path, Example, Tags, SuccessResponse, OperationId,  } from 'tsoa'
 import { CalculationService } from '../services/calculation.service'
 import { Invoice } from '../models'
 
@@ -12,6 +12,9 @@ export class InvoicesController extends Controller {
   }
 
   @Get(':id/Calculate')
+  @OperationId('Calculate Invoice')
+  @Tags('Invoices')
+  @SuccessResponse(200, 'Invoice total calculated', typeof Invoice)
   public async calculate(@Path('id') id: string): Promise<Invoice | null> {
     return this.calculationService.calculateInvoiceTotal(id)
   }
--- src/routes.ts
+++ src/routes.ts
@@ -75,7 +75,7 @@ export function RegisterRoutes(app: Router) {
                 response,
                 next,
                 validatedArgs,
-                successStatus: undefined,
+                successStatus: 200,
               });
             } catch (err) {
                 return next(err);
